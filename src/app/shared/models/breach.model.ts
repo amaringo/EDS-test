498223b7d3babd5eadc7251b8dc78f587a811b05
amaringo <osscro@gmail.com>
2019-05-16 11:39:51 +0700

    Auto-generated commit

--- src/app/shared/models/breach.model.ts
+++
@@ -1,9 +0,0 @@
-export class BreachModel {
-  constructor(
-    public Name: string,
-    public Title?: string,
-    public Domain?: string,
-    public LogoPath?: string,
-    public Description?: string,
-  ) {} // Полный список и описание на https://haveibeenpwned.com/API/v2#BreachModel
-}
